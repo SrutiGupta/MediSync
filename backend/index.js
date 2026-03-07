79b16fc52052b7bb8e6c08fcec1227de138beee7
SrutiGupta <mg6156710@gmail.com>
2026-03-07 10:52:51 +0530

    Finally dashboard section done

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,14 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.routes.js";
 import patientRoutes from "./routes/patient.routes.js";
 import bedRoutes from "./routes/bed.routes.js";
+import dashboardRoutes from "./routes/dash.routes.js"
 dotenv.config();
 connectDB();
 const app=express()
 app.use("/api/auth",authRoutes)
 app.use("/api/beds", bedRoutes);
 app.use("/api/patients", patientRoutes)
+app.use("/api/dashboard",dashboardRoutes)
 app.listen (process.env.PORT,()=>{
  console.log(`Server is running on port ${process.env.PORT}`)
 })
